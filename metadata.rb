823c98c165765b92de6731ef33b58221d077ec82
Steffen Gebert <steffen.gebert@typo3.org>
2013-03-15 16:37:46 +0100

    Version 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures gitweb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 %w{ build-essential mysql database java git maven apache2 }.each do |cookbook|
   depends cookbook
