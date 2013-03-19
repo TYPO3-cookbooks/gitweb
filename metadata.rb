51fd417f875662046307d956c6ce323203212713
Steffen Gebert <steffen.gebert@typo3.org>
2013-03-19 23:22:22 +0100

    Remove unused cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -5,12 +5,10 @@ description      "Installs/Configures gitweb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
 
-%w{ build-essential mysql database java git maven apache2 }.each do |cookbook|
-  depends cookbook
-end
 
 recipe "gitweb::default", "Installs and configures gitweb."
 
 supports "debian"
 
 depends "ssl_certificates"
+depends "apache2"
